b59b3ebedbe8e3419844094623a203842b8e04e4
rcbxd <rbcxdd@gmail.com>
2020-10-09 19:06:22 -0500

    hashing methods + generators

---
+++ src/util/encryption.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcryptjs");
+
+const CHARACTERS_ALLOWED = "abcdefghijklmnopqrstuvxyz1234567890-_".split("");
+
+generateLink = function (length) {
+    let out = "";
+    for (let i = 0; i < length; i++) {
+        out +=
+            CHARACTERS_ALLOWED[Math.floor(Math.random() * CHARACTERS_ALLOWED.length)];
+    }
+    return out;
+};
+
+generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+validatePassword = function (inputpass, dbpass) {
+    return bcrypt.compareSync(inputpass, dbpass);
+};
+
+module.exports = { generateLink, generateHash, validatePassword };
